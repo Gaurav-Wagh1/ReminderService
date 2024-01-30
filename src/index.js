cbe15d8dffc1d5226d39d069adc3a07b08221974
Gaurav-Wagh1 <gauravwagh0506@gmail.com>
2024-01-30 19:58:01 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/server-config');
-const {sendBasicEmail} = require('./services/email-service');
+const { sendBasicEmail } = require('./services/email-service');
+const cron = require('node-cron');
 
 const startServer = () => {
     const app = express();
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended : true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            // 'reminder.service.gw@gmail.com',
-            'terahastachehra@gmail.com',
-            'This is a testing email',
-            'Hey, this is a new topic in our syllabus'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     // 'reminder.service.gw@gmail.com',
+        //     'terahastachehra@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, this is a new topic in our syllabus'
+        // );
     });
 }
 
